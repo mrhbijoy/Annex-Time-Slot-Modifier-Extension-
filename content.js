63cac75adff15ca504f159ae5e9892d4fcd108c9
Hasan Bijoy <mrhbijoy@gmail.com>
2024-03-12 13:40:21 +0600

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
+const tables = [...document.querySelectorAll("[id=tableRtn]")];
 
-const timeSlots = document.querySelectorAll("th:not(:first-child)"); 
-
-let timeSlotsText = [
+tables.forEach((table) => {
+  const timeSlots = table.querySelectorAll("th");
+  let timeSlotsText = [
+    "p",
     "09:00 AM to 09:45 AM",
     "09:45 AM to 10:30 AM",
     "10:30 AM to 11:15 AM",
@@ -9,9 +11,27 @@ let timeSlotsText = [
     "12:00 PM to 12:45 PM",
     "12:45 PM to 01:30 PM",
     "02:00 PM to 02:45 PM",
-    "02:45 PM to 03:30 PM"
+    "02:45 PM to 03:30 PM",
+  ];
+
+  for (let i = 1; i < timeSlots.length && i < timeSlotsText.length; i++) {
+    timeSlots[i].textContent = timeSlotsText[i];
+  }
+});
+
+const timeSlots = document.querySelectorAll("th:not(:first-child)");
+
+let timeSlotsText = [
+  "09:00 AM to 09:45 AM",
+  "09:45 AM to 10:30 AM",
+  "10:30 AM to 11:15 AM",
+  "11:15 AM to 12:00 PM",
+  "12:00 PM to 12:45 PM",
+  "12:45 PM to 01:30 PM",
+  "02:00 PM to 02:45 PM",
+  "02:45 PM to 03:30 PM",
 ];
 
 for (let i = 0; i < timeSlots.length && i < timeSlotsText.length; i++) {
-    timeSlots[i].textContent = timeSlotsText[i];
+  timeSlots[i].textContent = timeSlotsText[i];
 }
